e2c2ab03e4e40b5d9bb7dd0a7472fa644aa36701
Carter Supple <Cartersupple123@gmail.com>
2021-10-07 22:06:43 -0700

    added arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,13 @@
 // Assignment code here
-// Add function
+var uppercase = ("Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M");
+var lowercase = ("q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l"
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -12,6 +17,11 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
